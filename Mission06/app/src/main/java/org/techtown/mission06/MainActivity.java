d5faf033419768cddc65ea5ee035078d0c8674bf
Hongdonggeon <ghd700@naver.com>
2021-04-29 18:58:59 +0900

    progress bar

---
+++ Mission06/app/src/main/java/org/techtown/mission06/MainActivity.java
@@ -0,0 +1,39 @@
+package org.techtown.mission06;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        EditText editText = findViewById(R.id.value_input);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                progressBar.setProgress(progress);
+                editText.setText(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
